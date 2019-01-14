3391bc9cbff8db248c7f605a6fa8d0844f37e3c6
jsuhoversnik <jake.suhoversnik@gmail.com>
2019-01-14 18:47:46 -0500

    wrote FizzBuzz program

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
 console.log("Introduction to Javascript");
+
+function fizzBuzz(){
+
+    for(var i = 1; i <=100; i++){
+        let result = "";
+
+        if(i % 3 === 0 || i % 5 === 0){
+            if(i % 3 === 0) {
+                result += "Fizz";
+            }
+            if (i % 5 === 0){
+                result += "Buzz";
+            }
+        }else{
+            result += i;
+        }
+        console.log(result);
+    }
+}
